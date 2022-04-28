b5c2505bbb209a60ffdaeff5dea13f990a8ff24a
PBOsx <pablo.souza76@etec.sp.gov.br>
2022-04-27 22:02:24 -0300

    teste de fila

---
+++ controleHospi/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleHospi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+              String opcao;
+              String recor, view, exit;
+              String novoCadastro, cadastrados;
+
+       
+
+                  Console.WriteLine("press 1 for new registration or");
+              Console.WriteLine("press 2 for view records");
+              Console.WriteLine("press q for Exit");
+              opcao = Console.ReadLine();
+
+              switch (opcao)
+              {
+                  case "1":
+                      recor =("");
+                      break;
+
+                  case "2":
+                      view = ("");
+                      break;
+
+                  case "q":
+                      break;
+
+
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
